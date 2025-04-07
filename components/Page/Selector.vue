ef1a8f15c12bfddf8caa453e67145b41ca4b8658
ll3006 <doublel3006@gmail.com>
2025-04-07 19:16:34 +0200

    fix(reader): properly implement rtl/ltr reading modes
    Previously only rtl was working properly. Now clicking on the page selector,
    using the arrow keys navigation and clicking on the image will all respect
    the current reading direction.

--- components/Page/Selector.vue
+++ components/Page/Selector.vue
@@ -5,9 +5,17 @@ const progress = useProgressStore();
 
 const images = computed(() => progress.source?.images);
 
-// Convert index to page value
+// Convert index (0-based) to page value (1-based)
+// depending on current reading direction
 function idxToPage(idx: number): number {
-  return progress.pageCount - idx;
+  switch (settings.readDirection) {
+    case "right-to-left":
+      return progress.pageCount - idx;
+    case "left-to-right":
+      return idx + 1;
+    default:
+      return idx;
+  }
 }
 
 function onPageSelect(idx: number) {
@@ -19,6 +27,7 @@ function onPageSelect(idx: number) {
   <div
     class="absolute bottom-0 flex w-full flex-row gap-0.5 overflow-hidden bg-gradient-to-t from-black transition-opacity"
     :class="[settings.pinPageSelector ? '' : tw`opacity-0 hover:opacity-100`]"
+    v-show="progress.pageCount > 1"
   >
     <span
       class="pointer-events-none absolute right-1/2 z-10 self-center align-middle text-sm font-bold text-white"
--- components/Reader/Horizontal.vue
+++ components/Reader/Horizontal.vue
@@ -11,23 +11,44 @@ const currentSrc = computed(() =>
   progress.source?.images.at(progress.page - 1)
 );
 
+// Define action based on current reading direction
+const action = computed(() => {
+  switch (settings.readDirection) {
+    case "left-to-right":
+      return {
+        left: progress.prev,
+        right: progress.next,
+      };
+    case "right-to-left":
+      return {
+        left: progress.next,
+        right: progress.prev,
+      };
+    default:
+      return {
+        left: () => {},
+        right: () => {},
+      };
+  }
+});
+
 function onClick(e: MouseEvent) {
   let w = (e.target as HTMLElement).offsetWidth;
   let rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
   let calc = w / 2 - e.offsetX;
   if (Math.abs(calc) > 2 * rem) {
-    if (calc > 0) progress.next();
-    else progress.prev();
+    if (calc > 0) action.value.left();
+    else action.value.right();
   }
 }
 
 function onKeydown(event: KeyboardEvent) {
   switch (event.key) {
     case "ArrowLeft":
-      progress.next();
+      action.value.left();
       break;
     case "ArrowRight":
-      progress.prev();
+      action.value.right();
       break;
     case "j":
       // TODO: toggle source
